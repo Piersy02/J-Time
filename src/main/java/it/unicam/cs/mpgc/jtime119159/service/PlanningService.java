7351d3ed3fbe9318c2f4a153ce39e5cc59e7849e
SAVERIO MARIA PIERSIGILLI <saveriom.piersigilli@studenti.unicam.it>
2026-02-09 21:28:53 +0100

    "definire le attività da svolgere in un giorno specifico, tenendo conto dell’impegno totale".

---
+++ src/main/java/it/unicam/cs/mpgc/jtime119159/service/PlanningService.java
@@ -0,0 +1,9 @@
+package it.unicam.cs.mpgc.jtime119159.service;
+
+import it.unicam.cs.mpgc.jtime119159.model.Task;
+import java.time.LocalDate;
+import java.util.List;
+
+public class PlanningService {
+
+}
---
+++ src/main/java/it/unicam/cs/mpgc/jtime119159/service/report/ReportStrategy.java
@@ -0,0 +1,9 @@
+package it.unicam.cs.mpgc.jtime119159.service.report;
+
+import it.unicam.cs.mpgc.jtime119159.model.Task;
+
+import java.util.List;
+
+public interface ReportStrategy {
+    String generate(List<Task> tasks);
+}
