f222dc3353f33060eed38eab53a60181c6511e3c
SAVERIO MARIA PIERSIGILLI <saveriom.piersigilli@studenti.unicam.it>
2026-02-10 13:16:49 +0100

    Eccezione Personalizzata Per gestire gli errori di business (es. "non puoi chiudere questo progetto"), creiamo una piccola eccezione.

---
+++ src/main/java/it/unicam/cs/mpgc/jtime119159/service/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package it.unicam.cs.mpgc.jtime119159.service.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
